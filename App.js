7301c6f6a8459a4909279e9a36d968b16c1d5f6f
Mahavir Vataliya <mahavir@infobizzs.com>
2019-04-10 13:41:14 +0530

    Form Builder Demo

---
+++ App.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import { AppRegistry, ScrollView, ActivityIndicator } from "react-native";
+import { View, Text, Button, StyleProvider } from "native-base";
+import GenerateForm from "@infobizzs/rn-form-builder";
+import getTheme from "./native-base-theme/components";
+import theme from "./form-theme";
+import LogicResolver from "./lib/logic-resolver";
+import customFields from './lib/custom-fields';
+import * as data from './data.json';
+
+const styles = {
+  container: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  wrapper: {
+    flex: 1,
+    marginTop: 80,
+  },
+  submitButton: {
+    paddingHorizontal: 10,
+    paddingTop: 20
+  }
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: false,
+      logics: false,
+      orginalForm: false
+    };
+  }
+
+  componentWillMount() {
+   
+  }
+
+  componentDidMount() {
+    // fetch("http://192.168.0.116:8000/example")
+    // .then(resp => resp.json())
+    // .then(response => {
+    //   this.setState({
+    //     form: response.fields,
+    //     logics: response.logic,
+    //     orginalForm: response.fields
+    //   });
+    //    this.onValueChange();
+    // });
+
+    const response = data;
+    this.setState({
+      form: response.fields,
+      logics: response.logic,
+      orginalForm: response.fields
+    });
+    setTimeout(() => {
+      this.onValueChange();
+    }, 100);
+  }
+
+  onValueChange() {
+    if (!this.formGenerator) {
+      return;
+    }
+    const formValues = this.formGenerator.getValues();
+    const logics = this.state.logics;
+    let fieldsToHide = [];
+
+    const logicResolver = new LogicResolver();
+    fieldsToHide = logicResolver.getFieldsToHide(
+      formValues,
+      logics,
+      this.state.form
+    );
+
+    // let make copy of originl form
+    let fields = JSON.parse(JSON.stringify(this.state.orginalForm));
+
+    // remove hidden fields from array
+    fields.forEach((item, index) => {
+      if (fieldsToHide.includes(item.key)) {
+        fields.splice(index, 1);
+      }
+    });
+
+    this.setState({ form: fields });
+  }
+
+  login() {
+    const formValues = this.formGenerator.getValues();
+    console.log("FORM VALUES", formValues);
+  }
+  render() {
+    if (!this.state.form)
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
+    return (
+      <StyleProvider style={getTheme()}>
+        <View style={styles.wrapper}>
+          <ScrollView>
+            <GenerateForm
+              ref={c => {
+                this.formGenerator = c;
+              }}
+              customComponents={customFields}
+              scrollViewProps={{ scrollEnabled: false }}
+              onValueChange={name => this.onValueChange(name)}
+              theme={theme}
+              fields={this.state.form}
+              logics={this.state.logics}
+              orginalForm={this.state.orginalForm}
+            />
+
+            <View style={styles.submitButton}>
+              <Button block onPress={() => this.login()}>
+                <Text>Submit</Text>
+              </Button>
+            </View>
+          </ScrollView>
+        </View>
+      </StyleProvider>
+    );
+  }
+}
---
+++ native-base-theme/components/Input.js
@@ -0,0 +1,18 @@
+// @flow
+
+import variable from './../variables/platform';
+
+export default (variables /*: * */ = variable) => {
+  const inputTheme = {
+    '.multiline': {
+      height: null,
+    },
+    height: variables.inputHeightBase,
+    color: variables.inputColor,
+    paddingBottom: 10,
+    flex: 1,
+    fontSize: variables.inputFontSize,
+  };
+
+  return inputTheme;
+};
